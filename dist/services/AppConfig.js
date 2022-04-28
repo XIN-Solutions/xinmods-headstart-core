bdcce26e7e969e177de2d25a68dad85e72402d2e
Marnix Kok <marnix@xinsolutions.co.nz>
2022-04-28 23:01:16 +1200

    Release build

--- dist/services/AppConfig.js
+++ dist/services/AppConfig.js
@@ -9,6 +9,7 @@ var configuration = {
   hotReload: process.env.HOT_RELOAD === "true",
   hotReloadPort: parseInt(process.env.HOT_RELOAD_PORT || "30303"),
   baseDomain: process.env.BASE_DOMAIN || 'frontend.local',
+  baseUrl: process.env.BASE_URL || 'http://frontend.local:3000',
   defaultPageTTL: 60,
   Frontend: {
     env: process.env.NODE_ENV || 'development',
